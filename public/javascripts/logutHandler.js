d6d3d38d29bd81292191427a483431e3065b63fd
mariiyamm <mary2802sh@gmail.com>
2026-01-15 15:41:26 +0100

    opprett bruker

---
+++ public/javascripts/logutHandler.js
@@ -0,0 +1,12 @@
+async function loggut(){
+    const response = await fetch("./logut", {
+        method: 'POST', 
+    });
+
+    if (response.ok) {
+        alert("Du er logget ut");
+        window.location.href = '/login';
+    } else {
+        alert ("Noe gikk galt");
+    }
+}
